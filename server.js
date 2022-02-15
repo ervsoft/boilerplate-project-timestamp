d30b9295347400a7d17c1556f4b331f0c440963c
ervsoft <ag81@aof.anadolu.edu.tr>
2022-02-15 16:48:50 +0300

    aster

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+//var validateDate = require("validate-date");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,6 +26,55 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api", function (req, res) {
+  
+      date = new Date()
+     const finaldateempty = { 
+            unix: date.getTime(), 
+            utc: date.toUTCString() 
+        }
+        console.log(finaldateempty)
+        return res.json(finaldateempty)
+  
+ });
+
+app.get('/api/:date', (req, res) => {
+          let date
+          date = !isNaN(req.params.date)
+          ? new Date(Number(req.params.date))
+          : new Date(req.params.date)
+  
+         // Recode edilecek //
+  const timeStamp = {
+        error : date.toUTCString(),
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    }
+    console.log(timeStamp)
+
+    res.json(timeStamp)
+  
+  /*
+    var ctime =  req.params.date;
+  if(ctime)
+    res.json({unix: qtime});
+
+  */
+
+   
+});
+
+
+
+app.get('/now', function(req, res, next) {
+req.time = new Date().toString();
+    next();
+}, function(req, res) {
+ res.send({
+      time: req.time
+    });
+
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
